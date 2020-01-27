4fc697adad0d57d104ec5e856eca9dcc2215a2b1
Damon <Pdamongreen1399@gmail.com>
2020-01-27 01:07:03 -0500

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Damon Andre Green on 1/27/20.
+//  Copyright © 2020 Damon Andre Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
